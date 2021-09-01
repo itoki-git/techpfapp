9355d58ce119b2965d4f9ab5de59e0df32fd4b4e
itoki-git <zxxgxdfggd@gmail.com>
2021-09-01 22:36:39 +0900

    feat: menu追加時の動作

--- frontend/components/molecules/Menu.jsx
+++ frontend/components/molecules/Menu.jsx
@@ -1,9 +1,23 @@
 import styles from "../../styles/Menu.module.scss";
+import { useSetRecoilState, useRecoilState } from "recoil";
+
+import { menuState, menuListState } from "../../pages/api/create";
+import { useState } from "react";
 
 const Menu = (props) => {
+  const [menu, setMenu] = useRecoilState(menuListState);
+  const [count, setCount] = useState(0);
+  const addItem = (menuid) => {
+    setMenu(() => menu.concat({ id: count, component: menuid }));
+  };
+
   return (
     <li className={styles.navname} key={props.id}>
-      <a className={`${styles["link"]} ${styles["menu"]}`} href="#">
+      <a
+        className={`${styles["link"]} ${styles["menu"]}`}
+        href="#"
+        onClick={() => (addItem(props.id), setCount(count + 1))}
+      >
         {props.displayName}
       </a>
     </li>
