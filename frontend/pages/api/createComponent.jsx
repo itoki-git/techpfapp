6aec5687d2015f60417f407fb7f923aa489ca577
itoki-git <zxxgxdfggd@gmail.com>
2021-09-12 11:37:49 +0900

    feat: 前後入れ替え処理

--- frontend/pages/api/createComponent.jsx
+++ frontend/pages/api/createComponent.jsx
@@ -3,21 +3,41 @@ import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import ClearIcon from "@material-ui/icons/Clear";
 import Grid from "@material-ui/core/Grid";
+import Tooltip from "@material-ui/core/Tooltip";
 import { useSetRecoilState, useRecoilState, useRecoilValue } from "recoil";
 
 import { Textarea } from "../../components/atoms/Textarea";
 import { Input } from "../../components/atoms/Input";
-import { menuListState } from "./createStore";
+import { menuListState, itemID } from "./createStore";
 
 import styles from "../../styles/Component.module.scss";
 
 export const SideButton = (props) => {
   const [menu, setMenu] = useRecoilState(menuListState);
+
   const deleteItem = () => {
     let result = menu.filter((item) => {
-      return item.id !== props.deleteID;
+      return item.id !== props.itemID;
     });
     setMenu(result);
+  };
+
+  const replacingOnItem = () => {
+    let swap = menu.slice();
+    let index = Number(props.swapID);
+    if (index > 0) {
+      swap.splice(index - 1, 2, swap[index], swap[index - 1]);
+      setMenu(swap);
+    }
+  };
+
+  const replacingUnderItem = () => {
+    let swap = menu.slice();
+    let index = Number(props.swapID);
+    if (index < menu.length - 1) {
+      swap.splice(index, 2, swap[index + 1], swap[index]);
+      setMenu(swap);
+    }
   };
 
   return (
@@ -28,15 +48,23 @@ export const SideButton = (props) => {
         justifyContent="flex-start"
         alignItems="flex-end"
       >
-        <IconButton onClick={() => deleteItem()}>
-          <ClearIcon fontSize="small" />
-        </IconButton>
-        <IconButton>
-          <ArrowUpwardIcon fontSize="small" />
-        </IconButton>
-        <IconButton>
-          <ArrowDownwardIcon fontSize="small" />
-        </IconButton>
+        <Tooltip title="削除" placement="right-end">
+          <IconButton onClick={() => deleteItem()}>
+            <ClearIcon fontSize="small" />
+          </IconButton>
+        </Tooltip>
+
+        <Tooltip title="上に移動" placement="right-end">
+          <IconButton onClick={() => replacingOnItem()}>
+            <ArrowUpwardIcon fontSize="small" />
+          </IconButton>
+        </Tooltip>
+
+        <Tooltip title="下に移動" placement="right-end">
+          <IconButton onClick={() => replacingUnderItem()}>
+            <ArrowDownwardIcon fontSize="small" />
+          </IconButton>
+        </Tooltip>
       </Grid>
     </Grid>
   );
