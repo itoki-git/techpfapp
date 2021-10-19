5922e7a23687ad210e22573560b95a8461b03b95
itoki-git <zxxgxdfggd@gmail.com>
2021-10-19 20:59:33 +0900

    feat: ローディング画面

--- frontend/components/templates/PrivateLayout.jsx
+++ frontend/components/templates/PrivateLayout.jsx
@@ -1,13 +1,18 @@
 import Head from "next/head";
-import Router from "next/router";
+import { useRouter } from "next/router";
 import axios from "axios";
 import useSWR from "swr";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import Header from "../organisms/Header";
 import { loginState } from "../state/currentUser";
 import styles from "../../styles/Layout.module.scss";
-import { headerMenuState } from "../state/componentStore";
+import {
+  headerMenuState,
+  fetchState,
+  loadState,
+} from "../state/componentStore";
 import { url, api } from "../../pages/api/utility";
+import Loading from "../organisms/Load";
 
 const PrivateLayout = (props) => {
   const { title, children } = props;
@@ -16,10 +21,16 @@ const PrivateLayout = (props) => {
   const fetcher = (url) => axios.post(url).then((res) => res.data);
   const { data, error } = useSWR(api.user, fetcher);
   const header = useRecoilValue(headerMenuState);
+  const router = useRouter();
+  const isReady = router.isReady;
+
+  if (!isReady || !data) {
+    return <Loading />;
+  }
 
   if (!data || error) {
     setLoginState(false);
-    Router.push(url.login);
+    router.push(url.login);
   } else {
     setLoginState(true);
   }
