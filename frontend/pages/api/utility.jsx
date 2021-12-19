dbd02532efb44731933394b1f00b7094e6f6c4ce
itoki-git <zxxgxdfggd@gmail.com>
2021-12-19 16:45:19 +0900

    すべてのページで使う変数など

--- frontend/pages/api/utility.jsx
+++ frontend/pages/api/utility.jsx
@@ -15,3 +15,14 @@ export const api = {
   user: 'api/user',
   register_article: 'api/registerArticle',
 };
+
+export const privateMenu = [
+  { id: '1', displayName: 'Logout', to: url.logout },
+  { id: '2', displayName: 'Article', to: url.article },
+  { id: '3', displayName: 'Create', to: url.create },
+  { id: '4', displayName: 'Page', to: url.demo },
+];
+export const publicMenu = [
+  { id: '1', displayName: 'Login', to: url.login },
+  { id: '2', displayName: 'Article', to: url.article },
+];
