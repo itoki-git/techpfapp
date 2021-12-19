4f72fbbcc815dcdead12b97481d2a0e12856ae19
itoki-git <zxxgxdfggd@gmail.com>
2021-12-19 16:44:27 +0900

    ログイン処理

--- frontend/components/organisms/Login.jsx
+++ frontend/components/organisms/Login.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Input } from '../atoms/Input';
 import { textStateFamily } from '../state/createStore';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 import styles from '../../styles/organisms/Login.module.scss';
 import { useRecoilState, useResetRecoilState } from 'recoil';
-import { loginState } from '../state/currentUser';
 import { api, url } from '../../pages/api/utility';
+import { AuthContext } from '../state/AuthStore';
 
 const Login = (props) => {
   const [email, setEmail] = useRecoilState(textStateFamily('email'));
   const [password, setPswd] = useRecoilState(textStateFamily('password'));
-  const setLoginState = useResetRecoilState(loginState);
+  const [message, setMessage] = useState('');
+  const [messageType, setMessageType] = useState('');
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+  const { state, dispatch } = useContext(AuthContext);
   const router = useRouter();
+
+  // Loginに成功or失敗したときにメッセージを表示する
+  const AlertBar = (props) => {
+    return (
+      <Alert variant="filled" elevation="filled" {...props}>
+        This is a success alert — check it out!
+      </Alert>
+    );
+  };
+  const handleAlertClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setIsAlertOpen(false);
+  };
 
   const login = async (e) => {
     const data = { email: email, password: password };
@@ -22,34 +42,74 @@ const Login = (props) => {
       .post(api.login, data, {
         withCredentials: true,
       })
-      .then((res) => console.log('LOGIN'), setLoginState(true), handler())
-      .catch((err) => setLoginState(false));
+      .then((res) => {
+        dispatch({
+          type: 'GET_NAME',
+          payload: res.data.name,
+        });
+        dispatch({
+          type: 'GET_EMAIL',
+          payload: res.data.email,
+        });
+        dispatch({
+          type: 'GET_SHORT_PROFILE',
+          payload: res.data.shortProfile,
+        });
+        dispatch({
+          type: 'GET_PROFILE',
+          payload: res.data.profile,
+        });
+        dispatch({
+          type: 'LOGIN_STATUS',
+          payload: true,
+        });
+      })
+      .catch((err) => {
+        dispatch({
+          type: 'LOGIN_STATUS',
+          payload: false,
+        });
+        setMessageType('error');
+        setMessage('Login failed.');
+        setIsAlertOpen(true);
+      });
   };
-  const handler = () => {
-    setLoginState(true);
-    setEmail('');
-    setPswd('');
+
+  if (state.isLogin) {
     router.push(url.article);
-  };
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.title}>Login</div>
-      <form onSubmit={login}>
-        <div className={styles.field}>
-          <label>Email</label>
-          <Input id="email" component="auth" type="text" />
-        </div>
-        <div className={styles.field}>
-          <label>Password</label>
-          <Input id="password" component="auth" type="password" />
-        </div>
-        <div className={styles.field}>
-          <input className={styles.submit} type="submit" value="Login" />
-        </div>
-        <div className={styles.signup}>
-          Not a member? <Link href={url.signup}>Signup now</Link>
-        </div>
-      </form>
+    <div>
+      <div className={styles.wrapper}>
+        <div className={styles.title}>Login</div>
+        <form onSubmit={login}>
+          <div className={styles.field}>
+            <label>Email</label>
+            <Input id="email" component="auth" type="text" />
+          </div>
+          <div className={styles.field}>
+            <label>Password</label>
+            <Input id="password" component="auth" type="password" />
+          </div>
+          <div className={styles.field}>
+            <input className={styles.submit} type="submit" value="Login" />
+          </div>
+          <div className={styles.signup}>
+            Not a member? <Link href={url.signup}>Signup now</Link>
+          </div>
+        </form>
+      </div>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={isAlertOpen}
+        autoHideDuration={6000}
+        onClose={handleAlertClose}
+      >
+        <Alert variant="filled" severity={messageType} sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
