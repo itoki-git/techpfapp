9de89fc44696fc9e0cecc2330517df7be2a39f19
itoki-git <zxxgxdfggd@gmail.com>
2021-12-22 01:53:44 +0900

    カーソルの位置にボタンの文字列を挿入

--- frontend/components/molecules/SideButton.jsx
+++ frontend/components/molecules/SideButton.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Stack from '@mui/material/Stack';
 import ImageIcon from '@mui/icons-material/Image';
 import AddLinkIcon from '@mui/icons-material/AddLink';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import Fab from '@mui/material/Fab';
 import { Input } from '@mui/material';
+import { useRecoilState } from 'recoil';
+import { textStateFamily } from '../state/createStore';
 
 const fabColorStyle = {
   color: 'common.black',
@@ -13,23 +15,36 @@ const fabColorStyle = {
     bgcolor: '#fff',
   },
 };
+const fabs = [
+  {
+    color: 'inherit',
+    sx: { ...fabColorStyle },
+    icon: <AddLinkIcon />,
+    label: 'Expand',
+    text: '[title](url)',
+  },
+  {
+    color: 'inherit',
+    sx: { ...fabColorStyle },
+    icon: <QuestionMarkIcon />,
+    label: 'Expand',
+  },
+];
 
-export const SideButton = () => {
-  const fabs = [
-    {
-      color: 'inherit',
-      sx: { ...fabColorStyle },
-      icon: <AddLinkIcon />,
-      label: 'Expand',
-    },
-    {
-      color: 'inherit',
-      sx: { ...fabColorStyle },
-      icon: <QuestionMarkIcon />,
-      label: 'Expand',
-    },
-  ];
+export const SideButton = (props) => {
+  const [text, setText] = useRecoilState(textStateFamily(props.id));
 
+  const insertInButton = (inner) => {
+    console.log(inner);
+    const editorEl = document.getElementById('editEl');
+    const sentence = editorEl.value;
+    const index = editorEl.selectionStart;
+    editorEl.value = sentence.substring(0, index) + inner + sentence.substring(index, sentence.length);
+    editorEl.focus();
+    const newString = index + inner.length;
+    editorEl.setSelectionRange(newString, newString);
+    setText(editorEl.value);
+  };
   return (
     <Stack direction="column" justifyContent="center" alignItems="center" spacing={4}>
       <label htmlFor="icon-button-file">
@@ -39,7 +54,7 @@ export const SideButton = () => {
         </Fab>
       </label>
       {fabs.map((fab, index) => (
-        <Fab sx={fab.sx} aria-label={fab.label} color={fab.color}>
+        <Fab sx={fab.sx} aria-label={fab.label} color={fab.color} onClick={() => insertInButton(fab.text)}>
           {fab.icon}
         </Fab>
       ))}
