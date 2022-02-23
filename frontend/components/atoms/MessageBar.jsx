e87dce0d93d02e334e9d998b8fd0140dd29e354a
itoki-git <zxxgxdfggd@gmail.com>
2022-02-24 03:28:09 +0900

    成功や失敗などバーとして表示するコンポーネント

---
+++ frontend/components/atoms/MessageBar.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export const MessageSnackbar = (props) => {
+  const [state, setState] = useState(props.barState);
+
+  const { vertical, horizontal, open, message, severity } = state;
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{ vertical, horizontal }}
+        open={open}
+        autoHideDuration={4000}
+        onClose={props.messageBarClose}
+        key={vertical + horizontal}
+      >
+        <Alert onClose={props.messageBarClose} severity={severity} sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+};
