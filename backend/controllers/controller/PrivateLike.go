0b760b2eff0fa53e21faf82df3535f678a072fe5
itoki-git <zxxgxdfggd@gmail.com>
2022-04-13 02:57:49 +0900

    いいね押したらLikesテーブルにデータを追加・削除する

--- backend/controllers/controller/PrivateLike.go
+++ backend/controllers/controller/PrivateLike.go
@@ -4,6 +4,7 @@ import (
 	"app/models/db"
 	"app/models/entity"
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -14,32 +15,38 @@ import (
 
 func UpdateLike(ctx *gin.Context) {
 	var like entity.LikeUser
+	var user entity.LikeUser
 	var result bool
-	postID := ctx.Params.ByName("id")
-	userID := ctx.GetString("userID")
-	id, _ := primitive.ObjectIDFromHex(userID)
+	getPostID := ctx.Params.ByName("id")
+	getUserID := ctx.GetString("userID")
+	fmt.Println(getPostID, getUserID)
+	id, _ := primitive.ObjectIDFromHex(getUserID)
+	postID, _ := primitive.ObjectIDFromHex(getPostID)
 	checkFilter := bson.M{"articleID": postID, "users._id": id}
 	filter := bson.M{"articleID": postID}
-	update := bson.M{"users._id": id}
+
 	// LIKEテーブルに存在するかチェック
-	isErr := LikeCollection.FindOne(context.TODO(), checkFilter)
+	isErr := LikeCollection.FindOne(context.TODO(), checkFilter).Decode(&user)
 
 	like.UserID = id
 	like.Timestamp = time.Now()
 
-	if isErr != nil {
-		_, err := LikeCollection.UpdateOne(context.TODO(), filter, bson.M{"$set": update})
-		if err != nil {
-			db.GetError(err, ctx)
-			return
-		}
-		result = true
-	} else {
-		if _, err := LikeCollection.DeleteOne(context.TODO(), checkFilter); err != nil {
+	update := bson.M{"users": like}
+
+	if isErr == nil {
+		// 記事のいいねを削除する
+		if _, err := LikeCollection.UpdateOne(context.TODO(), filter, bson.M{"$pull": bson.M{"users": bson.M{"_id": id}}}); err != nil {
 			db.GetError(err, ctx)
 			return
 		}
 		result = false
+	} else {
+		// 記事のいいねに追加する
+		if _, err := LikeCollection.UpdateOne(context.TODO(), filter, bson.M{"$push": update}); err != nil {
+			db.GetError(err, ctx)
+			return
+		}
+		result = true
 	}
 	ctx.JSON(http.StatusOK, gin.H{"liked": result})
 }
--- backend/models/db/connect.go
+++ backend/models/db/connect.go
@@ -60,6 +60,23 @@ func ConnectPostsDB() *mongo.Collection {
 	return collection
 }
 
+func ConnectLikesDB() *mongo.Collection {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	// 指定したURIに接続する
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI"))
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to Likes Collection. ")
+
+	collection := client.Database(dbname).Collection("likes")
+
+	return collection
+}
+
 // GetError エラーを返却する
 func GetError(err error, ctx *gin.Context) {
 	var response = ErrorResponse{
